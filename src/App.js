9696ce295a68175f2b8ca34a906f74b332e114e9
박호연 <qkrghdus1113@naver.com>
2022-11-02 16:49:17 +0900

    221102 - 16:49 리액트 블로그 만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
+import axios from 'axios'
+import { useState, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 function App() {
+  let [test, setTest] = useState('하이용')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+      <Route path="/" element={
+        <>
+
+ <h1>블로그 입니다. {test}</h1>
+ <h1>블로그 입니다. 2</h1>
+ <h1>블로그 입니다 3</h1>
+ <h1>블로그 입니다 4</h1>
+ <button onClick={()=>{
+  let copy = test;
+  copy = copy + '채린앙'
+  setTest(copy)
+  
+ }}
+ >변경스테이트 써보자잉</button>
+</>
+      }></Route>
+      <Route path="/light" element={<div>이게 라우튼데요?</div>}></Route>
+     </Routes>
+    
+   
+
     </div>
   );
 }
